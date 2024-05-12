cfbe908f47ba81db97d6b20586fe75918a5ceba1
코인 <juniffel8318@gmail.com>
2024-05-13 06:14:35 +0900

    z

--- 바이비트/tele.py
+++ 바이비트/tele.py
@@ -1,19 +1,21 @@
 import telegram 
 from telegram.constants import ParseMode
+import traceback
 
 async def  tele_bot(message, reply=None): #실행시킬 함수명 임의지정
     """
     텔레그램 메세지 전송 
     message :  전송할 메세지 str
     """
-    # token
-    with open('/home/joon/바탕화면/coin/바이비트/알트토큰.txt') as f:
-        lines = f.readlines()
-        token = lines[0].strip()
-        id = lines[1].strip()
-        
-    bot = telegram.Bot(token = token)
-    await bot.send_message(chat_id=id, text=message, parse_mode = ParseMode.HTML, reply_markup = reply)
-
-
-
+    try:
+        # token
+        with open('/home/joon/바탕화면/coin/바이비트/알트토큰.txt') as f:
+            lines = f.readlines()
+            token = lines[0].strip()
+            id = lines[1].strip()
+            
+        bot = telegram.Bot(token = token)
+        await bot.send_message(chat_id=id, text=message, parse_mode = ParseMode.HTML, reply_markup = reply)
+    except Exception as e:
+        err_msg = traceback.format_exc()
+        print(err_msg)
